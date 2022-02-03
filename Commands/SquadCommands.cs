6c96c8295cdc560533a7a3322664e1c2bcbbb892
ChristosLabrou <chris@labrou.gr>
2022-02-03 11:34:20 +0200

    vscode commit

--- Commands/SquadCommands.cs
+++ Commands/SquadCommands.cs
@@ -10,45 +10,73 @@ using System.Collections.Generic;
 namespace JayBot.Commands
 {
     public class SquadCommands : BaseCommandModule{
-        [Command("squad")]
+        [Command("guild")]
         [Description("wip")]
-        public async Task SquadSubmit(CommandContext ctx, string mode, string name, params string[] members){
+        public async Task SquadSubmit(CommandContext ctx, string mode, params string[] parameters){
             bool found = false;
-            if (mode.ToLower()=="submit"){
+            string replyText = "";
+            if (mode.ToLower()=="submit" || mode.ToLower()=="add"){
+                //We assume that guild name is parameters[0]
                 List<string> tempList = new List<string>();
-                for (int i=0;i<members.Length;i++){
-                    tempList.Add(members[i]);
+                //replyText += $"Squad {parameters[0]} added with members:\n";
+                for(int i=1;i<parameters.Length;i++){
+                    tempList.Add(parameters[i]);
+                    replyText+="<@"+parameters[i]+">\n";
                 }
-                Bot.squads.Add(new Squad(name, tempList));
+                Bot.squads.Add(new Squad(parameters[0], tempList, replyText));
+
                 string output = Newtonsoft.Json.JsonConvert.SerializeObject(Bot.squads);
 				File.WriteAllText(Bot.squadJsonPath, output);
-                await ctx.Channel.SendMessageAsync("Squad added").ConfigureAwait(false);
+                await ctx.Channel.SendMessageAsync($"Guild {parameters[0]} added with members:\n"+replyText).ConfigureAwait(false);
             }
-            else if (mode.ToLower()=="summon" || mode.ToLower()=="call"){
-                string replyText = "";
-                for(int i =0; i<Bot.squads.Count;i++){
-                    if (Bot.squads[i].name == name){
+            else if (mode.ToLower()=="summon" || mode.ToLower()=="call" || mode.ToLower()=="ping"){
+                for (int i=0;i<Bot.squads.Count;i++){
+                    if (Bot.squads[i].name==parameters[0]){
                         found = true;
-                        for (int j=0;j<Bot.squads[i].memberIDs.Count;j++){
-                            replyText += "<@" + Bot.squads[i].memberIDs[j] + "> ";
+                        foreach(var member in Bot.squads[i].memberIDs){
+                            if (ctx.Member.Id.ToString()==member){
+                                await ctx.Channel.SendMessageAsync($"Guild {Bot.squads[i].name} you're summoned!\n"+Bot.squads[i].massPing).ConfigureAwait(false);
+                                break;
+                            }
                         }
-                        replyText += "\nYou are summoned to wengaem";
                         break;
                     }
                 }
-                if(!found){
-                    replyText = "Squad not found. Check for typos and if the squad exists. If it doesn't you can submit one";
+
+                if (!found){
+                    await ctx.Channel.SendMessageAsync("Guild not found. Either you're dumdum and fucked up the name or the guild doesn't exist").ConfigureAwait(false);
                 }
-                await ctx.Channel.SendMessageAsync(replyText).ConfigureAwait(false);
             }
             else if (mode.ToLower()=="list"){
-                string replyText = "";
                 for(int i =0; i<Bot.squads.Count;i++){
-                    replyText += Bot.squads[i].name;
+                    replyText += $"{i+1}) "+Bot.squads[i].name+"\n";
                 }
                 await ctx.Channel.SendMessageAsync(replyText).ConfigureAwait(false);
             }
-
+            else if (mode.ToLower()=="delete"){
+                bool isMember = false;
+                foreach(var guild in Bot.squads){
+                    if (guild.name == parameters[0]){
+                        foreach(var member in guild.memberIDs){
+                            if (ctx.Member.Id.ToString() == member){
+                                isMember = true;
+                                Bot.squads.Remove(guild);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+                if(!isMember){
+                    replyText = "Access denied. Only members can delete a guild";
+                }
+                else{
+                    replyText = "Guild deleted";
+                }
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(Bot.squads);
+				File.WriteAllText(Bot.squadJsonPath, output);
+                await ctx.Channel.SendMessageAsync(replyText).ConfigureAwait(false);
+            }
         }
 
     }
--- Squad.cs
+++ Squad.cs
@@ -5,11 +5,19 @@ namespace JayBot{
     public class Squad{
         public string name;
         public List<string> memberIDs;
+        public string massPing {get;set;}
 
-        [JsonConstructor]
+        //[JsonConstructor]
         public Squad(string name, List<string> memberIDs){
             this.name = name;
             this.memberIDs = memberIDs;
+        }
+
+        [JsonConstructor]
+        public Squad(string name, List<string> memberIDs, string massPing){
+            this.name = name;
+            this.memberIDs = memberIDs;
+            this.massPing = massPing;
         }
     }
 }
